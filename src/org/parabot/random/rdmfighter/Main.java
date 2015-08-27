d32b581378b63d8b5f76d0fa6ac636d032bbb501
Kendal Lane <legends.master333@gmail.com>
2015-08-27 16:29:12 +0200

    Paint variable renaming

--- src/org/parabot/random/rdmfighter/Main.java
+++ src/org/parabot/random/rdmfighter/Main.java
@@ -14,6 +14,7 @@ import org.parabot.environment.scripts.Script;
 import org.parabot.environment.scripts.ScriptManifest;
 import org.parabot.environment.scripts.framework.Strategy;
 import org.rev317.min.api.wrappers.Npc;
+import org.parabot.random.rdmfighter.data.Methods;
 import org.parabot.random.rdmfighter.data.Variables;
 import org.parabot.random.rdmfighter.strategies.Attack;
 import org.parabot.random.rdmfighter.strategies.Eat;
@@ -38,7 +39,7 @@ public class Main extends Script implements Paintable {
 
 	private String enemyInformation;
 	private String foodInformation;
-	private int biggestWidth = -1;
+	private int biggestStringWidth = -1;
 	
 	
 	@Override
@@ -89,53 +90,51 @@ public class Main extends Script implements Paintable {
 	}
 	
 	@Override
-	public void paint(Graphics Graphs) {
+	public void paint(Graphics g) {
 		if(startScript) {
-			try {
-				Graphics2D g = (Graphics2D) Graphs;
-				Color orange = new Color(255, 152, 31);
+			Graphics2D graphs = (Graphics2D) g;
+			Color orange = new Color(255, 152, 31);
 				
-				if(biggestWidth == -1) {
-					int enemyStringWidth = (int) g.getFontMetrics().getStringBounds(enemyInformation, g).getWidth();
-					int foodStringWidth = (int) g.getFontMetrics().getStringBounds(foodInformation, g).getWidth();
-					biggestWidth = ((enemyStringWidth > foodStringWidth) ? enemyStringWidth : foodStringWidth);
-				}
+			if(biggestStringWidth == -1) {
+				int enemyStringWidth = (int) graphs.getFontMetrics().getStringBounds(enemyInformation, graphs).getWidth();
+				int foodStringWidth = (int) graphs.getFontMetrics().getStringBounds(foodInformation, graphs).getWidth();
+				biggestStringWidth = ((enemyStringWidth > foodStringWidth) ? enemyStringWidth : foodStringWidth);
+			}
 				
-				g.setColor(new Color(0, 0, 0, 160));
-				g.fillRect(0, 0, 112, 34);
-				g.fillRect(0, 34, 47 + biggestWidth, 31);
+			graphs.setColor(new Color(0, 0, 0, 160));
+			graphs.fillRect(0, 0, 112, 34);
+			graphs.fillRect(0, 34, 47 + biggestStringWidth, 31);
 				
-				int Y1 = 16;
-				int Y2 = 48;
+			int Y1 = 16;
+			int Y2 = 48;
 				
-				drawDString(Manifest.name() + " v" + Manifest.version(), 6, Y1, g, orange);
-				drawDString("Runtime: " + scriptTimer.toString(), 6, Y1 + 13, g, orange);
+			drawDoubleString(Manifest.name() + " v" + Manifest.version(), 6, Y1, graphs, orange);
+			drawDoubleString("Runtime: " + scriptTimer.toString(), 6, Y1 + 13, graphs, orange);
 	
-				drawDString("Killing:", 6, Y2, g, orange);
-				drawDString("Food:", 6, Y2 + 13, g, orange);
-				drawDString(enemyInformation, 45, Y2, g, Color.white);
-				drawDString(foodInformation, 45, Y2 + 13, g, Color.white);
+			drawDoubleString("Killing:", 6, Y2, graphs, orange);
+			drawDoubleString("Food:", 6, Y2 + 13, graphs, orange);
+			drawDoubleString(enemyInformation, 45, Y2, graphs, Color.white);
+			drawDoubleString(foodInformation, 45, Y2 + 13, graphs, Color.white);
 				
-				Npc fightingNPC = Methods.getAttackingNPC();
-				if(fightingNPC != null && fightingNPC.getMaxHealth() > 0) {
-					double percentHealth = ((double) fightingNPC.getHealth()) / fightingNPC.getMaxHealth();
-					g.setColor(new Color(0, 0, 0, 160));
-					g.fillRect(0, 70, 160, 25);
-					g.setColor(Color.red);
-					g.fillRect(6, 76, 148, 13);
-					g.setColor(new Color(0, 153, 0));
-					g.fillRect(6, 76, (int) (((double) 148) * percentHealth), 13);
-					if(fightingNPC.getHealth() != 0) {
-						drawDString(fightingNPC.getHealth() + " / " + fightingNPC.getMaxHealth() + " HP", 10, 87, g, Color.white);
-					} else {
-						drawDString("(DEAD)", 10, 87, g, Color.white);
-					}
+			Npc currentlyFightingNpc = Methods.getAttackingNPC();
+			if(currentlyFightingNpc != null && currentlyFightingNpc.getMaxHealth() > 0) {
+				double percentHealth = ((double) currentlyFightingNpc.getHealth()) / currentlyFightingNpc.getMaxHealth();
+				graphs.setColor(new Color(0, 0, 0, 160));
+				graphs.fillRect(0, 70, 160, 25);
+				graphs.setColor(Color.red);
+				graphs.fillRect(6, 76, 148, 13);
+				graphs.setColor(new Color(0, 153, 0));
+				graphs.fillRect(6, 76, (int) (((double) 148) * percentHealth), 13);
+				if(currentlyFightingNpc.getHealth() != 0) {
+					drawDoubleString(currentlyFightingNpc.getHealth() + " / " + currentlyFightingNpc.getMaxHealth() + " HP", 10, 87, graphs, Color.white);
+				} else {
+					drawDoubleString("(DEAD)", 10, 87, graphs, Color.white);
 				}
-			} catch (Exception _e) {}
+			}
 		}
 	}
 	
-	public void drawDString(String text, int x, int y, Graphics2D g, Color col) {
+	public void drawDoubleString(String text, int x, int y, Graphics2D g, Color col) {
 		g.setColor(Color.black);
 		g.drawString(text, x + 1, y + 1);
 		g.setColor(new Color(255, 152, 31));
